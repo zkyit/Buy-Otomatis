7813a49fe1a0ed15c0df972579551f1412e031c5
IbzzXdzz <stockibzzstore@gmail.com>
2025-02-23 09:07:36 +0700

    Add files via upload

---
+++ system/utilityService.js
@@ -0,0 +1,13 @@
+function generateRandomPassword(length = 12) {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    return password;
+}
+
+module.exports = {
+    generateRandomPassword
+};
